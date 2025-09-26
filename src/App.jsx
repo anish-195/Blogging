9a7e4fe195bf07ad8d4a6ad1365d85a11e837e64
Anish <anish052004@gmail.com>
2025-09-26 14:35:52 +0530

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import config from './config/config.js'
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice.js'
+import { Footer, Header } from './components/index.js';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      <h1>{config.appwriteProjectId}</h1>
+
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) =>{
+      if(userData){
+        dispatch(login({userData}));
+      }else{
+        dispatch(logout());
+      }
+     
+    })  
+    .finally(()=> setLoading(false));
+  }, []);
+   
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between  bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main> 
+        <Footer />
+      </div>
     </div>
-  )
+  ) : null 
 }
 
 export default App
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>Bye</h1>
+    </div>
+  )
+}
+
+export default Footer
